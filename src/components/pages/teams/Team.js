d4d5214c2c8e500a3846e9dd9a79f4b723280bd5
Zach Smith <zsmith126028@gmail.com>
2019-06-09 16:17:59 -0500

    add initial component for Team

---
+++ src/components/pages/teams/Team.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Team extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This page will be the profile layout for each team</h1>
+      </div>
+    );
+  }
+}
+
+export default Team;
