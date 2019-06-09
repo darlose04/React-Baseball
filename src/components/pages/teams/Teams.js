412b2741f1c27175a173fce549c446efe7363ebe
Zach Smith <zsmith126028@gmail.com>
2019-06-09 16:17:06 -0500

    add initial page for Team listings

---
+++ src/components/pages/teams/Teams.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Teams extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          This will be the page that lists every team according to league and
+          division
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Teams;
