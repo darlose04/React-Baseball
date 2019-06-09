6324e2b3c10f175887ce19722a3945980f655b44
Zach Smith <zsmith126028@gmail.com>
2019-06-09 13:25:13 -0500

    remove link to App.css for now

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/layout/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import "./App.css";
+// import "./App.css";
 
 function App() {
   return (
