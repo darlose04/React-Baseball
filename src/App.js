49db5f937f925a3d5fe5d3dfa717d31e8dd27ef5
Zach Smith <zsmith126028@gmail.com>
2019-06-09 13:45:24 -0500

    change App to class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // import "./App.css";
 
-function App() {
-  return (
-    <Router>
-      <div className='App'>
-        <Navbar />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <Navbar />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
