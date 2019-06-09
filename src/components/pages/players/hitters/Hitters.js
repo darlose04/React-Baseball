8c83dbe9272530016ec4482bcbc4beca95c7fbc2
Zach Smith <zsmith126028@gmail.com>
2019-06-09 16:12:33 -0500

    add basic Hitters component

---
+++ src/components/pages/players/hitters/Hitters.js
@@ -0,0 +1,14 @@
+// this will be the page that maps through and displays a hitter item for each hitter. It will be a table
+import React, { Component } from "react";
+
+class Hitters extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the page that will display the hitters in a table</h1>
+      </div>
+    );
+  }
+}
+
+export default Hitters;
