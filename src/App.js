0337bfcf2ea95c5992555cad949466b5dd117c2a
Zach Smith <zsmith126028@gmail.com>
2019-06-09 10:17:27 -0500

    import react-router-dom. import Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
-  return <div className='App' />;
+  return (
+    <Router>
+      <div className='App'>
+        <Navbar />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
