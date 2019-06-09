e0d8c0c5660fac2f351b4f1667040cc31ebfe547
Zach Smith <zsmith126028@gmail.com>
2019-06-09 17:49:12 -0500

    remoe a bunch of stuff to restart

--- src/App.js
+++ src/App.js
@@ -5,35 +5,11 @@ import axios from "axios";
 
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-import Players from "./components/pages/Players";
-import Hitters from "./components/pages/players/hitters/Hitters";
-
-import Teams from "./components/pages/teams/Teams";
 
 // import "./App.css";
 
 class App extends Component {
-  state = {
-    hitters: [],
-    hitter: {},
-    starters: [],
-    starter: {},
-    relievers: [],
-    reliever: {},
-    loading: false
-  };
-
-  // get hitters from api
-  getHitters = async () => {
-    this.setState({ loading: true });
-
-    const res = await axios.get("http://localhost:4000/batting2018");
-
-    this.setState({ hitters: res.data, loading: false });
-  };
-
   render() {
-    const { loading, hitters } = this.state;
     return (
       <Router>
         <div className='App'>
@@ -42,15 +18,6 @@ class App extends Component {
             <Switch>
               <Route exact path='/' component={Home} />
               <Route exact path='/about' component={About} />
-              <Route exact path='/players' component={Players} />
-              <Route exact path='/teams' component={Teams} />
-              <Route
-                exact
-                path='/hitters'
-                render={props => (
-                  <Hitters loading={loading} hitters={hitters} />
-                )}
-              />
             </Switch>
           </div>
         </div>
