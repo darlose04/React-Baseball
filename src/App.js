2fbe0916562ab51846cd02bc00c5f63c8cb9c771
Zach Smith <zsmith126028@gmail.com>
2019-06-09 16:10:59 -0500

    add Player and Hitter routes to APp

--- src/App.js
+++ src/App.js
@@ -5,10 +5,28 @@ import axios from "axios";
 
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Players from "./components/pages/Players";
+import Hitters from "./components/pages/players/hitters/Hitters";
 
 // import "./App.css";
 
 class App extends Component {
+  state = {
+    hitters: [],
+    starters: [],
+    relievers: [],
+    loading: false
+  };
+
+  // get hitters from api
+  getHitters = async () => {
+    this.setState({ loading: true });
+
+    const res = await axios.get("http://localhost:4000/batting2018");
+
+    this.setState({ hitters: res.data, loading: false });
+  };
+
   render() {
     return (
       <Router>
@@ -18,6 +36,8 @@ class App extends Component {
             <Switch>
               <Route exact path='/' component={Home} />
               <Route exact path='/about' component={About} />
+              <Route exact path='/players' component={Players} />
+              <Route exact path='/hitters' component={Hitters} />
             </Switch>
           </div>
         </div>
