2640eae57b2a3b39ff9ca7eaf2b61bed126b815a
Zach Smith <zsmith126028@gmail.com>
2019-06-09 13:25:29 -0500

    add bootstrap to navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,31 +4,51 @@ import { Link } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
-      <nav className='navbar bg-primary'>
-        <h2>
-          <i className='fas fa-baseball-ball mr-2' />
-          Baseball Stats
-        </h2>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/teams'>Teams</Link>
-          </li>
-          <li>
-            <Link to='/players'>Players</Link>
-          </li>
-          <li>
-            <Link to='/standings'>Standings</Link>
-          </li>
-          <li>
-            <a href='https://www.reddit.com/r/baseball/'>r/baseball</a>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-        </ul>
+      <nav
+        className='navbar navbar-expand-sm navbar-dark bg-primary mb-3'
+        style={{ color: "#fff" }}
+      >
+        <div className='container'>
+          <h2>
+            <i className='fas fa-baseball-ball mr-2' />
+            Baseball Stats
+          </h2>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <Link className='nav-link text-light' to='/'>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link text-light' to='/teams'>
+                Teams
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link text-light' to='/players'>
+                Players
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link text-light' to='/standings'>
+                Standings
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='nav-link text-light'
+                href='https://www.reddit.com/r/baseball/'
+              >
+                r/baseball
+              </a>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link text-light' to='/about'>
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
