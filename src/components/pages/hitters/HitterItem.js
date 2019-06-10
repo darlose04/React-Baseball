e0ec531ab1cf9610dd52cbee28b396dbc21cce33
Zach Smith <zsmith126028@gmail.com>
2019-06-10 09:20:41 -0500

    change to a functional component to clean up code a bit

--- src/components/pages/hitters/HitterItem.js
+++ src/components/pages/hitters/HitterItem.js
@@ -1,62 +1,60 @@
-import React, { Component } from "react";
+import React from "react";
 
-class HitterItem extends Component {
-  render() {
-    const {
-      name,
-      team,
-      games,
-      plate_appearances,
-      homeruns,
-      runs,
-      rbi,
-      sb,
-      bb_perc,
-      k_perc,
-      iso,
-      babip,
-      avg,
-      obp,
-      slg,
-      ops,
-      wOBA,
-      wRC_plus,
-      bsr,
-      off,
-      def,
-      war
-    } = this.props.hitter;
+const HitterItem = props => {
+  const {
+    name,
+    team,
+    games,
+    plate_appearances,
+    homeruns,
+    runs,
+    rbi,
+    sb,
+    bb_perc,
+    k_perc,
+    iso,
+    babip,
+    avg,
+    obp,
+    slg,
+    ops,
+    wOBA,
+    wRC_plus,
+    bsr,
+    off,
+    def,
+    war
+  } = props.hitter;
 
-    return (
-      <tbody>
-        <tr>
-          {/* <th>{i++}</th> */}
-          <td>{name}</td>
-          <td>{team}</td>
-          <td>{games}</td>
-          <td>{plate_appearances}</td>
-          <td>{runs}</td>
-          <td>{homeruns}</td>
-          <td>{rbi}</td>
-          <td>{sb}</td>
-          <td>{bb_perc}</td>
-          <td>{k_perc}</td>
-          <td>{iso}</td>
-          <td>{babip}</td>
-          <td>{avg}</td>
-          <td>{obp}</td>
-          <td>{slg}</td>
-          <td>{ops}</td>
-          <td>{wOBA}</td>
-          <td>{wRC_plus}</td>
-          <td>{bsr}</td>
-          <td>{off}</td>
-          <td>{def}</td>
-          <td>{war}</td>
-        </tr>
-      </tbody>
-    );
-  }
-}
+  return (
+    <tbody>
+      <tr>
+        {/* <th>{i++}</th> */}
+        <td>{name}</td>
+        <td>{team}</td>
+        <td>{games}</td>
+        <td>{plate_appearances}</td>
+        <td>{runs}</td>
+        <td>{homeruns}</td>
+        <td>{rbi}</td>
+        <td>{sb}</td>
+        <td>{bb_perc}</td>
+        <td>{k_perc}</td>
+        <td>{iso}</td>
+        <td>{babip}</td>
+        <td>{avg}</td>
+        <td>{obp}</td>
+        <td>{slg}</td>
+        <td>{ops}</td>
+        <td>{wOBA}</td>
+        <td>{wRC_plus}</td>
+        <td>{bsr}</td>
+        <td>{off}</td>
+        <td>{def}</td>
+        <td>{war}</td>
+      </tr>
+    </tbody>
+  );
+};
 
 export default HitterItem;
