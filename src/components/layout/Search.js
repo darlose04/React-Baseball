6c1e810fcd9e10d01ce9dbe396d85f3c5eaa9ef5
Zach Smith <zsmith126028@gmail.com>
2019-06-11 15:34:02 -0500

    add state and onChange

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  state = {
+    text: ""
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <div>
-        <form className='form-inline ml-auto'>
+        <form onSubmit={this.onSubmit} className='form-inline ml-auto'>
           <input
             className='form-control mr-2'
             type='text'
             name='text'
             placeholder='Search Players...'
+            value={this.state.text}
+            onChange={this.onChange}
           />
           <input type='submit' value='Search' className='btn btn-dark' />
         </form>
