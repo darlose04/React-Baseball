98162cc7994790723afee8a247889cb3fd6767e8
Zach Smith <zsmith126028@gmail.com>
2019-06-16 20:47:40 -0500

    add react-bootstrap-table. may not need HitterItem component anymore. Also will need to see what happens when I refactor to use hooks and context

--- src/components/pages/hitters/Hitters.js
+++ src/components/pages/hitters/Hitters.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 
 import HitterItem from "./HitterItem";
 import Spinner from "../../layout/Spinner";
@@ -8,6 +9,7 @@ const Hitters = ({ hitters, loading }) => {
   if (loading) {
     return <Spinner />;
   } else {
+    /*
     return (
       <div>
         <table
@@ -45,6 +47,36 @@ const Hitters = ({ hitters, loading }) => {
           ))}
         </table>
       </div>
+    );
+    */
+    return (
+      <BootstrapTable data={hitters} striped hover>
+        <TableHeaderColumn isKey dataField='player_id'>
+          Player ID
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField='name'>Name</TableHeaderColumn>
+        <TableHeaderColumn dataField='team'>Team</TableHeaderColumn>
+        <TableHeaderColumn dataField='games'>Games</TableHeaderColumn>
+        <TableHeaderColumn dataField='plate_appearances'>PA</TableHeaderColumn>
+        <TableHeaderColumn dataField='runs'>Runs</TableHeaderColumn>
+        <TableHeaderColumn dataField='homeruns'>HR</TableHeaderColumn>
+        <TableHeaderColumn dataField='rbi'>RBI</TableHeaderColumn>
+        <TableHeaderColumn dataField='sb'>SB</TableHeaderColumn>
+        <TableHeaderColumn dataField='bb_perc'>BB %</TableHeaderColumn>
+        <TableHeaderColumn dataField='k_perc'>K %</TableHeaderColumn>
+        <TableHeaderColumn dataField='iso'>ISO</TableHeaderColumn>
+        <TableHeaderColumn dataField='babip'>BABIP</TableHeaderColumn>
+        <TableHeaderColumn dataField='avg'>AVG</TableHeaderColumn>
+        <TableHeaderColumn dataField='obp'>OBP</TableHeaderColumn>
+        <TableHeaderColumn dataField='slg'>SLG</TableHeaderColumn>
+        <TableHeaderColumn dataField='ops'>OPS</TableHeaderColumn>
+        <TableHeaderColumn dataField='wOBA'>wOBA</TableHeaderColumn>
+        <TableHeaderColumn dataField='wRC_plus'>wRC+</TableHeaderColumn>
+        <TableHeaderColumn dataField='bsr'>BsR</TableHeaderColumn>
+        <TableHeaderColumn dataField='pff'>Off</TableHeaderColumn>
+        <TableHeaderColumn dataField='def'>Def</TableHeaderColumn>
+        <TableHeaderColumn dataField='war'>WAR</TableHeaderColumn>
+      </BootstrapTable>
     );
   }
 };
