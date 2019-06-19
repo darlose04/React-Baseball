9a7c93be6dd9810719cb44b49ae022172ac0067e
Zach Smith <zsmith126028@gmail.com>
2019-06-19 16:00:07 -0500

    add Search to Home component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
+import Search from "../layout/Search";
+
 class Home extends Component {
   render() {
     return (
       <div>
-        <h1>This is the home page.</h1>
+        <Search />
       </div>
     );
   }
