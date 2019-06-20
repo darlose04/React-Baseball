7689cef4d73494d9ca07e99fa63a08b7dcce017a
Zach Smith <zsmith126028@gmail.com>
2019-06-19 20:45:28 -0500

    add HitterSearch component to be separate from Hitters component

---
+++ src/components/pages/searches/HitterSearch.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+import HitterItem from "../hitters/HitterItem";
+import Spinner from "../../layout/Spinner";
+import PropTypes from "prop-types";
+
+const HitterSearch = ({ hitterSearch, loading }) => {
+  if (hitterSearch.length === 0) {
+    return <div />;
+  } else if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div>
+        <table
+          className='table table-sm table-striped table-bordered table-hover table-responsive-sm'
+          style={{ fontSize: "12px" }}
+        >
+          <thead className='thead-dark'>
+            <tr>
+              <th>Name</th>
+              <th>Year</th>
+              <th>Team</th>
+              <th>Games</th>
+              <th>PA</th>
+              <th>Runs</th>
+              <th>HR</th>
+              <th>RBI</th>
+              <th>SB</th>
+              <th>BB%</th>
+              <th>K%</th>
+              <th>ISO</th>
+              <th>BABIP</th>
+              <th>AVG</th>
+              <th>OBP</th>
+              <th>SLG</th>
+              <th>OPS</th>
+              <th>wOBA</th>
+              <th>wRC+</th>
+              <th>BsR</th>
+              <th>Off</th>
+              <th>Def</th>
+              <th>WAR</th>
+            </tr>
+          </thead>
+
+          {hitterSearch.map(hitter => (
+            <HitterItem key={hitter.player_id} hitter={hitter} />
+          ))}
+        </table>
+      </div>
+    );
+  }
+};
+
+HitterSearch.propTypes = {
+  hitterSearch: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
+
+export default HitterSearch;
