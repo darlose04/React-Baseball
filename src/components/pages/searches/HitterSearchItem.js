3206a7f31fc21f400d684600ae46cd5bd5b044d4
Zach Smith <zsmith126028@gmail.com>
2019-06-19 21:01:59 -0500

    add new file to provide table for hitter searches

---
+++ src/components/pages/searches/HitterSearchItem.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const HitterSearchItem = props => {
+  const {
+    year,
+    team,
+    games,
+    plate_appearances,
+    homeruns,
+    runs,
+    rbi,
+    sb,
+    bb_perc,
+    k_perc,
+    iso,
+    babip,
+    avg,
+    obp,
+    slg,
+    ops,
+    wOBA,
+    wRC_plus,
+    bsr,
+    off,
+    def,
+    war
+  } = props.hitter;
+
+  return (
+    <tbody>
+      <tr>
+        <td>{year}</td>
+        <td>{team}</td>
+        <td>{games}</td>
+        <td>{plate_appearances}</td>
+        <td>{runs}</td>
+        <td>{homeruns}</td>
+        <td>{rbi}</td>
+        <td>{sb}</td>
+        <td>{bb_perc}</td>
+        <td>{k_perc}</td>
+        <td>{iso}</td>
+        <td>{babip}</td>
+        <td>{avg}</td>
+        <td>{obp}</td>
+        <td>{slg}</td>
+        <td>{ops}</td>
+        <td>{wOBA}</td>
+        <td>{wRC_plus}</td>
+        <td>{bsr}</td>
+        <td>{off}</td>
+        <td>{def}</td>
+        <td>{war}</td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default HitterSearchItem;
