a1ef91dc983aab20aefa22bba5fdb9ec887a787a
Zach Smith <zsmith126028@gmail.com>
2019-06-22 18:02:53 -0500

    add Pitchers component to show pitchers table and map through PitcherItem

---
+++ src/components/pages/pitchers/Pitchers.js
@@ -0,0 +1,61 @@
+import React from "react";
+// import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+// import BootstrapTable from "react-bootstrap-table-next";
+
+import PitcherItem from "./PitcherItem";
+import Spinner from "../../layout/Spinner";
+import PropTypes from "prop-types";
+
+const Pitchers = ({ pitchers, loading }) => {
+  if (pitchers.length === 0) {
+    return <div />;
+  } else if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div>
+        <table
+          className='table table-sm table-striped table-bordered table-hover table-responsive-sm'
+          style={{ fontSize: "12px" }}
+        >
+          <thead className='thead-dark'>
+            <tr>
+              <th>Name</th>
+              <th>Year</th>
+              <th>Team</th>
+              <th>Wins</th>
+              <th>Losses</th>
+              <th>Saves</th>
+              <th>Games</th>
+              <th>Starts</th>
+              <th>IP</th>
+              <th>K/9</th>
+              <th>BB/9</th>
+              <th>HR/9</th>
+              <th>BABIP</th>
+              <th>LOB %</th>
+              <th>GB %</th>
+              <th>HR/FB</th>
+              <th>ERA</th>
+              <th>WHIP</th>
+              <th>FIP</th>
+              <th>xFIP</th>
+              <th>WAR</th>
+            </tr>
+          </thead>
+
+          {pitchers.map(pitcher => (
+            <PitcherItem key={pitcher.player_id} pitcher={pitcher} />
+          ))}
+        </table>
+      </div>
+    );
+  }
+};
+
+Pitchers.propTypes = {
+  pitchers: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
+
+export default Pitchers;
