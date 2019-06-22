132ec5b4b747ec8149f240c15a518824a0136c37
Zach Smith <zsmith126028@gmail.com>
2019-06-22 18:20:07 -0500

    create component to select year for Pitcher route. Going to need to find a way to do this so I only use one component for both hitters and pitchers

---
+++ src/components/layout/PitcherYearSelect.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class PitcherYearSelect extends Component {
+  state = {
+    year: ""
+  };
+
+  static propTypes = {
+    getPitcherYear: PropTypes.func.isRequired,
+    clearYear: PropTypes.func.isRequired,
+    showTableClear: PropTypes.bool.isRequired
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.getPitcherYear(this.state.year);
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { showTableClear, clearYear } = this.props;
+    return (
+      <div className='my-3'>
+        <form onSubmit={this.onSubmit} className='d-inline form-inline ml-auto'>
+          <input
+            className='form-control mr-2'
+            type='text'
+            name='year'
+            placeholder='Input Year (2014-2018)'
+            value={this.state.year}
+            onChange={this.onChange}
+          />
+          <input
+            type='submit'
+            value='Submit'
+            className='btn btn-outline-primary'
+          />
+        </form>
+        {showTableClear && (
+          <button
+            className='d-inline btn btn-outline-dark ml-2'
+            onClick={clearYear}
+          >
+            Clear
+          </button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default PitcherYearSelect;
