5ee8f2818827257a2bcb87de7401b681d08076e7
Zach Smith <zsmith126028@gmail.com>
2019-06-22 19:42:24 -0500

    add component for standings

---
+++ src/components/pages/standings/Standings.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Standings extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the standings page</h1>
+        <p>It will display yearly standings for the different divisions</p>
+      </div>
+    );
+  }
+}
+
+export default Standings;
