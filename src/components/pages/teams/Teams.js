53729fb3392efd90e0d72c06d58b67d4389396b7
Zach Smith <zsmith126028@gmail.com>
2019-06-22 19:40:45 -0500

    add component for Teams

---
+++ src/components/pages/teams/Teams.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Teams extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the teams page</h1>
+        <p>It will display team stat totals as well as the player stats</p>
+      </div>
+    );
+  }
+}
+
+export default Teams;
