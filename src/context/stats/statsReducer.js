252397eb0a3c86964b6b1569f692d016ae7a8efd
Zach Smith <zsmith126028@gmail.com>
2019-06-22 23:20:53 -0500

    create initial statsReducer, import types, create default case in switch

---
+++ src/context/stats/statsReducer.js
@@ -0,0 +1,14 @@
+import {
+  SEARCH_HITTERS,
+  GET_HITTER_YEAR,
+  CLEAR_PLAYERS,
+  CLEAR_YEAR,
+  SET_LOADING
+} from "../types";
+
+export default (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
