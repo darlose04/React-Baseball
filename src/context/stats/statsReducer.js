49474c4536d5eea61a05027fa7273845126dbdf3
Zach Smith <zsmith126028@gmail.com>
2019-06-22 23:24:44 -0500

    add cases for SET_LOADING and SEARCH_HITTERS

--- src/context/stats/statsReducer.js
+++ src/context/stats/statsReducer.js
@@ -8,6 +8,17 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case SEARCH_HITTERS:
+      return {
+        ...state,
+        hitterSearch: action.payload,
+        loading: false
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
     default:
       return state;
   }
