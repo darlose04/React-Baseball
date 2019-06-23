5d2f4c66611e1f2c69e502de0ceade22c144f780
Zach Smith <zsmith126028@gmail.com>
2019-06-22 21:32:50 -0500

    refactor SearchPitchers to use hooks

--- src/components/layout/SearchPitchers.js
+++ src/components/layout/SearchPitchers.js
@@ -1,57 +1,52 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-class PitcherSearch extends Component {
-  state = {
-    text: ""
-  };
+const PitcherSearch = ({ searchPitchers, clearPlayers, showClear }) => {
+  const [text, setText] = useState("");
 
-  static propTypes = {
-    searchPitchers: PropTypes.func.isRequired,
-    clearPlayers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired
-  };
-
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
-    this.props.searchPitchers(this.state.text);
+    searchPitchers(text);
 
-    this.setState({ text: "" });
+    setText("");
   };
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onChange = e => {
+    setText(e.target.value);
   };
 
-  render() {
-    const { clearPlayers, showClear } = this.props;
-    return (
-      <div className='my-5'>
-        <form onSubmit={this.onSubmit}>
-          <div className='form-group'>
-            <input
-              className='form-control'
-              type='text'
-              name='text'
-              placeholder='Search Players...'
-              value={this.state.text}
-              onChange={this.onChange}
-            />
-          </div>
+  return (
+    <div className='my-5'>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
           <input
-            type='submit'
-            value='Search Players'
-            className='btn btn-primary btn-block mb-2'
+            className='form-control'
+            type='text'
+            name='text'
+            placeholder='Search Players...'
+            value={text}
+            onChange={onChange}
           />
-        </form>
-        {showClear && (
-          <button className='btn btn-block btn-light' onClick={clearPlayers}>
-            Clear Player
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+        </div>
+        <input
+          type='submit'
+          value='Search Players'
+          className='btn btn-primary btn-block mb-2'
+        />
+      </form>
+      {showClear && (
+        <button className='btn btn-block btn-light' onClick={clearPlayers}>
+          Clear Player
+        </button>
+      )}
+    </div>
+  );
+};
+
+PitcherSearch.propTypes = {
+  searchPitchers: PropTypes.func.isRequired,
+  clearPlayers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired
+};
 
 export default PitcherSearch;
