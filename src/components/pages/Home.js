b155e095b2aab186fbbd7dd0a44e05321898baa5
Zach Smith <zsmith126028@gmail.com>
2019-07-02 15:55:02 -0500

    bring in Link so links work

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <h2 className='mt-2'>Site Features:</h2>
       <div className='list-group'>
-        <a href='/teams' className='list-group-item list-group-item-action'>
+        <Link to='/teams' className='list-group-item list-group-item-action'>
           <div className='d-flex w-100 justify-content-between'>
             <h3 className='mb-1'>Teams</h3>
           </div>
@@ -14,8 +15,8 @@ const Home = () => {
             year. Displays team totals as well as players stats.
           </p>
           <small>Not implemented yet.</small>
-        </a>
-        <a href='/hitters' className='list-group-item list-group-item-action'>
+        </Link>
+        <Link to='/hitters' className='list-group-item list-group-item-action'>
           <div className='d-flex w-100 justify-content-between'>
             <h3 className='mb-1'>Hitters</h3>
           </div>
@@ -25,8 +26,8 @@ const Home = () => {
             is accounted for in the database. Searching by year returns the
             statistics of all hitters for that season.
           </p>
-        </a>
-        <a href='/pitchers' className='list-group-item list-group-item-action'>
+        </Link>
+        <Link to='/pitchers' className='list-group-item list-group-item-action'>
           <div className='d-flex w-100 justify-content-between'>
             <h3 className='mb-1'>Pitchers</h3>
           </div>
@@ -37,8 +38,11 @@ const Home = () => {
             statistics of all pitchers for that season.
           </p>
           <small className='text-muted'>Includes starters and relievers.</small>
-        </a>
-        <a href='/standings' className='list-group-item list-group-item-action'>
+        </Link>
+        <Link
+          to='/standings'
+          className='list-group-item list-group-item-action'
+        >
           <div className='d-flex w-100 justify-content-between'>
             <h3 className='mb-1'>Standings</h3>
           </div>
@@ -47,7 +51,7 @@ const Home = () => {
             well as playoff series.
           </p>
           <small className='text-muted'>Not implemented yet.</small>
-        </a>
+        </Link>
       </div>
     </div>
   );
